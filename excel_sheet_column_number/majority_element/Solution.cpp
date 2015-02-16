f8b54f593146ad1f85ff2095556ba16b0e8794f8
沐泊 <haoquan.bai@alibaba-inc.com>
2015-02-16 15:46:27 +0800

    add files

---
+++ excel_sheet_column_number/majority_element/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int> &num) {
+        int retNum = 0;
+        int curCount = 0;
+        for (int i=0; i<num.size(); i++) {
+            if (curCount == 0) {
+                retNum = num[i];
+                curCount++;
+            } else {
+                if (num[i] == retNum) {
+                    curCount++;
+                } else {
+                    curCount--;
+                }
+            }
+        }
+        
+        return retNum;
+    }
+};
